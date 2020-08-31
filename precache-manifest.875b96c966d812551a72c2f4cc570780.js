f39bb0af66e404a52d497b83fab7f21f556043aa
Cathy Walker <cathylouisewalker@gmail>
2020-08-31 16:23:27 +0100

    Updates

---
+++ precache-manifest.875b96c966d812551a72c2f4cc570780.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776eb3e35a23ddba320695f70a6b01f6",
+    "url": "/cathywalker/index.html"
+  },
+  {
+    "revision": "d5ba11ffa6d12ac9b574",
+    "url": "/cathywalker/static/css/main.295425ff.chunk.css"
+  },
+  {
+    "revision": "5541a560e57935c2408a",
+    "url": "/cathywalker/static/js/2.e489f3f5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cathywalker/static/js/2.e489f3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ba11ffa6d12ac9b574",
+    "url": "/cathywalker/static/js/main.9f8d7153.chunk.js"
+  },
+  {
+    "revision": "082b998e785d5c966d89",
+    "url": "/cathywalker/static/js/runtime-main.7fc967e4.js"
+  },
+  {
+    "revision": "ab96d89e113d8e231673952f5555209e",
+    "url": "/cathywalker/static/media/accidental-theatre.ab96d89e.png"
+  },
+  {
+    "revision": "2b250fa401c93e2e7fe4a63ff478ce30",
+    "url": "/cathywalker/static/media/bbc-history.2b250fa4.png"
+  },
+  {
+    "revision": "8d4556da51f0990c43f00d29869615cf",
+    "url": "/cathywalker/static/media/bbc-homepage.8d4556da.png"
+  },
+  {
+    "revision": "9dbbe59a57108d58622141a369cdba5d",
+    "url": "/cathywalker/static/media/bbc-learning.9dbbe59a.png"
+  },
+  {
+    "revision": "c09170a628ef09ce7bccdd67677157f7",
+    "url": "/cathywalker/static/media/myc_podcast_icon.c09170a6.png"
+  },
+  {
+    "revision": "56528f6f15dd214589a46fb7257c332f",
+    "url": "/cathywalker/static/media/shieldmaiden-studios.56528f6f.png"
+  },
+  {
+    "revision": "bf68b7c6b9f78101e2bf742e41aafcb0",
+    "url": "/cathywalker/static/media/wii_podcast_deirdre_ep8.bf68b7c6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9eac7776d82ab41d13110a18791554df.js"
+  "/cathywalker/precache-manifest.875b96c966d812551a72c2f4cc570780.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cathywalker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
