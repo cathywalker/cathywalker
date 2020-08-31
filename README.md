d786a9f93d74edc8d7a2b06418bbd1b85de9864e
cathy <@cathylouisewalker@gmail.com>
2020-08-31 13:23:49 +0100

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-A Portfolio website
-
-Made in React
---
+++ precache-manifest.3575c815cc5c88ebd75fb7f9d036f5e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0590a3ddcab013c4a5bbd867d323b364",
+    "url": "/cathywalker/index.html"
+  },
+  {
+    "revision": "903b8a0a0b71b7c9ee0d",
+    "url": "/cathywalker/static/css/main.295425ff.chunk.css"
+  },
+  {
+    "revision": "438582582c5ac8ce0e24",
+    "url": "/cathywalker/static/js/2.d984a30d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cathywalker/static/js/2.d984a30d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "903b8a0a0b71b7c9ee0d",
+    "url": "/cathywalker/static/js/main.f01111dd.chunk.js"
+  },
+  {
+    "revision": "082b998e785d5c966d89",
+    "url": "/cathywalker/static/js/runtime-main.7fc967e4.js"
+  }
+]);
